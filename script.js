d6dd30b76382467226e6c5b582a737810d61282d
GABRIEL CARVALHO LORENSSETTI <gabriel_lorenssetti@estudante.sesisenai.org.br>
2024-09-16 11:21:42 -0300

    upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function enviarValor() {
     let numeroEnviarVariavel = document.getElementById("number").value
 
+    alert("O número que você digitou foi: "+numeroEnviarVariavel+"!")
+
     localStorage.setItem("Número", numeroEnviarVariavel)
+
+
 }
